39e15dfb07f43acb7e9bd8cb6fbf16cd37277d7a
coolfish <461836324@qq.com>
2014-11-14 00:04:47 +0800

    update

--- www/js/app.js
+++ www/js/app.js
@@ -67,6 +67,7 @@ angular.module('starter', ['ionic', 'starter.controllers'])
       }
     });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  /*首页*/
+  $urlRouterProvider.otherwise('/app/browse');
 });
 
